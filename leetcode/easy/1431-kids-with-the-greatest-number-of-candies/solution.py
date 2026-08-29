d2a8024e5afb8be2e795945477ee15624e090f4a
Gaurinandana S <gaurinandanas4@gmail.com>
2026-08-29 09:40:42 +0530

    ✅ Kids With the Greatest Number of Candies | Easy | Python | Runtime: 0 ms (100.00%) | Memory: 19.1 MB | 2026-08-29

---
+++ leetcode/easy/1431-kids-with-the-greatest-number-of-candies/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_candies = max(candies)
+        result = [False] * len(candies)
+        for i in range(0, len(candies)):
+            if candies[i] + extraCandies >= max_candies:
+                result[i] = True
+        return result
